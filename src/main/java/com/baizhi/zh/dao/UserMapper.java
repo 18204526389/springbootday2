998fa094d857fc546371b2a9259fdcf6cfd180d1
zh <zh1356818115@163.com>
2019-04-17 09:39:12 +0800

    success

--- src/main/java/com/baizhi/zh/dao/UserMapper.java
+++ src/main/java/com/baizhi/zh/dao/UserMapper.java
@@ -1,8 +1,15 @@
 package com.baizhi.zh.dao;
 
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.baizhi.zh.entity.User;
+import org.springframework.jdbc.core.SqlOutParameter;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.sql.SQLOutput;
+
 public interface UserMapper extends Mapper<User> {
+
+
+
 }
